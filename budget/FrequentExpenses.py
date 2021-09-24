8a1f466647813f0a9b5796df4b0d98e0549ac408
Jose Vicente Nunez <josevnz@kodegeek.com>
2021-09-24 10:58:49 -0400

    Commit for PluralSight Project

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,6 +1,10 @@
+import collections
+from pathlib import Path
+from os import path
 import Expense
 
-SPENDING_DATA = "data/spending_data.csv"
+PARENT_DIR = Path(__file__).parent.parent
+SPENDING_DATA = path.join(PARENT_DIR, 'data', 'spending_data.csv')
 
 expense = Expense.Expenses()
 expense.read_expenses(SPENDING_DATA)
@@ -8,3 +12,5 @@ expense.read_expenses(SPENDING_DATA)
 spending_categories = []
 for expense in expense.list:
     spending_categories.append(expense.category)
+spending_counter = collections.Counter(spending_categories)
+print(spending_counter)
