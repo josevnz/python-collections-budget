6cd97473481e6294d3200880c943b7766aaf20d0
Jose Vicente Nunez <josevnz@kodegeek.com>
2021-09-24 11:16:06 -0400

    Commit for PluralSight Project

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -2,6 +2,7 @@ import collections
 from pathlib import Path
 from os import path
 import Expense
+import matplotlib.pyplot as plt
 
 PARENT_DIR = Path(__file__).parent.parent
 SPENDING_DATA = path.join(PARENT_DIR, 'data', 'spending_data.csv')
@@ -14,3 +15,10 @@ for expense in expense.list:
     spending_categories.append(expense.category)
 spending_counter = collections.Counter(spending_categories)
 print(spending_counter)
+top5 = spending_counter.most_common(5)
+print(f"Most frequent: {top5}")
+(fig, ax) = plt.subplots()
+(categories, count) = zip(*top5)
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+plt.show()
