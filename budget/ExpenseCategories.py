d9645b9596400d6bd233a70477f50d244617159c
Jose Vicente Nunez <josevnz@kodegeek.com>
2021-09-26 07:24:48 -0400

    Commit for PluralSight Project

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,3 +1,4 @@
+import timeit
 from pathlib import Path
 from os import path
 
@@ -30,6 +31,18 @@ def main():
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
+    print(timeit.timeit(
+        stmt="expenses.categorize_for_loop()",
+        setup='''
+from budget import Expense        
+expenses = Expense.Expenses()
+PARENT_DIR = Path(__file__).parent.parent
+SPENDING_DATA = path.join(PARENT_DIR, 'data', 'spending_data.csv')
+expenses.read_expenses(SPENDING_DATA)        
+''',
+        number=1000,
+        globals=globals()
+    ))
 
 
 if __name__ == "__main__":
